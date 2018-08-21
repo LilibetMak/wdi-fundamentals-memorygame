20b85c79dfa3da5b6d4a0be621df65586d6e75f9
Liz Makawa <lizmakawa@Lizs-MBP.fios-router.home>
2018-08-20 20:32:01 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+  {
+    card: 'queen',
+    suit: 'hearts',
+    cardImage: 'images/queen-of-hearts.png'
+  },
+  {
+    card: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png'
+  },
+  {
+    card: 'king',
+    suit: 'hearts',
+    cardImage: 'images/king-of-hearts.png'
+  },
+  {
+    card: 'king',
+    suit: 'diamonds',
+    cardImage: 'images/king-of-diamonds.png'
+  }
+];
 
 var cardsInPlay = [];
 
@@ -13,9 +34,13 @@ var checkForMatch = function () {
 
 var flipCard = function (cardId) {
 
-  console.log("User flipped " + cards[cardId]);
+  console.log("User flipped " + cards[cardId].card);
 
-  cardsInPlay.push(cards[cardId]);
+  cardsInPlay.push(cards[cardId].card);
+
+  console.log(cards[cardId].cardImage);
+
+  console.log(cards[cardId].suit);
 
   if (cardsInPlay.length === 2) {
     checkForMatch();
